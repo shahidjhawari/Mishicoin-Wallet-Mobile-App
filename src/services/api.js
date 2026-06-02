fd04430468efa95e2f36efaea3dce494bdf02ce1
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2026-06-03 03:42:49 +0500

    refactor: remove backend code for Mishicoin project and update API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.166:5000/api",
+  baseURL: "https://mishicoin-mobile-app-backend.vercel.app/",
 });
 
 export default API;
