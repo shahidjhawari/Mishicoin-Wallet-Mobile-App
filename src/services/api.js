97b2386c8010f16eea895f66b1c94b2c20712763
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2026-06-03 16:32:12 +0500

    fix: update API base URL to include '/api' and add npm start command for cache reset

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mishicoin-mobile-app-backend.vercel.app/",
+  baseURL: "https://mishicoin-mobile-app-backend.vercel.app/api",
 });
 
 export default API;
